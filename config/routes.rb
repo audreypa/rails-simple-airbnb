b4e1ffa05869c08cb027e68fdfd2de6a94de21ea
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-14 16:38:22 +0200

    model Flat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'flat/name:string'
-  get 'flat/address:string'
-  get 'flat/description:text'
-  get 'flat/price_per_night:integer'
-  get 'flat/number_of_guests:integer'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
